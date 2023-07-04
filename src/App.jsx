4a14a09b664ddb0c82fe822c41f68c2681643a83
Yordan Beltran <yordanbb98@gmail.com>
2023-07-04 10:06:23 -0400

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { useRef } from 'react'
 import './App.css'
 import { Movies } from './Components/movies'
 
 function App() {
+  const inputRef = useRef()
+  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+  }
+
   return (
     <div className='page'>
       <header>
         <h1>Movie Finder</h1>
-        <form action="" className='form'>
-          <input type="text" placeholder='Avengers, Star Wars, The Matrix ...' />
+        <form action="" className='form' onSubmit={handleSubmit}>
+          <input ref={inputRef} type="text" placeholder='Avengers, Star Wars, The Matrix ...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
